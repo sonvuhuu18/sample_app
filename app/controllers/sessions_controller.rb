c38aa245bf52fca44a7c074e2e6f1db4d7ec033c
sonvuhuu18 <son.vuhuu18@gmail.com>
2019-07-08 10:09:58 +0700

    Chuong 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,28 @@ class SessionsController < ApplicationController
     user = User.find_by email: session[:email].downcase
 
     if user && user.authenticate(session[:password])
-      log_in user
-      session[:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        session[:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t "login.warning"
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = t "login.invalid"
-      render :new
+      unauthorized_log_in
     end
   end
 
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def unauthorized_log_in
+    flash.now[:danger] = t "login.invalid"
+    render :new
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      log_in @user
-      flash[:success] = t "signup.success"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "signup.info"
+      redirect_to root_url
     else
       render :new
     end
