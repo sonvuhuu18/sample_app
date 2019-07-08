cf9e19fd6bed50a7a765fb607622825f21aa2190
sonvuhuu18 <son.vuhuu18@gmail.com>
2019-07-08 10:09:58 +0700

    Chuong 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find params[:id]
+  before_action :logged_in_user, only: %i(index edit update)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.all.page(params[:page]).per Settings.users_per_page
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -19,10 +26,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "user.edit.success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t "user.delete.success"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
       :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "user.not_logged_in"
+    redirect_to login_url
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:danger] = t "user.not_found"
+    redirect_to root_url
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
